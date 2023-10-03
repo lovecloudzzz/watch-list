3d2ed0c6d78288a26a7a4c8c95d67c2cf450539f
Дмитрий Моисеенко <90276559+lovecloudzzz@users.noreply.github.com>
2023-10-03 17:22:35 +0300

    asd commit xd

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-
 use tauri::{Manager, Window};
 use tauri_plugin_sql::{Builder, Migration, MigrationKind};
 
@@ -54,4 +53,3 @@ fn main() {
                     .run(tauri::generate_context!())
                     .expect("error while running tauri application")
 }
-
